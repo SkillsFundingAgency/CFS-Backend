c04fcac46050300ae31712e4deb63f351a53e05b
Matt Hammond <matt@talkthinkdo.co.uk>
2017-10-27 14:14:45 +0100

    Update health checks

--- Functions/Allocations.Functions.Results/HealthCheck.cs
+++ Functions/Allocations.Functions.Results/HealthCheck.cs
@@ -1,10 +1,12 @@
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
 
 namespace Allocations.Functions.Results
 {
@@ -13,22 +15,8 @@ namespace Allocations.Functions.Results
         [FunctionName("HealthCheck")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
-
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
-
-            // Get request body
-            dynamic data = await req.Content.ReadAsAsync<object>();
-
-            // Set name to query string or body data
-            name = name ?? data?.name;
-
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            var versionNumber = Assembly.GetExecutingAssembly().GetName().Version;
+            return req.CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(new { versionNumber }));
         }
     }
 }
