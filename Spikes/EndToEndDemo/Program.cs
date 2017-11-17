6fa3f2b28b90f03d31ae3cb44bf04f8ce0d6d557
Matt Hammond <matt@talkthinkdo.co.uk>
2017-11-17 12:45:16 +0000

    Changes to id system

--- Spikes/EndToEndDemo/Program.cs
+++ Spikes/EndToEndDemo/Program.cs
@@ -64,7 +64,7 @@ namespace EndToEndDemo
                     Console.ReadKey();
                 }
 
-                await StoreAggregates(budgetDefinition, new AllocationFactory(compilerOutput.Assembly));
+               // await StoreAggregates(budgetDefinition, new AllocationFactory(compilerOutput.Assembly));
 
 
 
@@ -83,136 +83,6 @@ namespace EndToEndDemo
             }
         }
 
-        private static async Task StoreAggregates(Budget budget, AllocationFactory allocationFactory)
-        {
-            using (var aggregateRepository = new Repository<AggregateDataset>("datasets"))
-            {
-                using (var repository = new Repository<ProviderSourceDataset>("datasets"))
-                {
-                    {
- 
-                        var typeNames = allocationFactory.GetDatasetTypeNames();
-                        foreach (var typeName in typeNames)
-                        {
-                            var type = allocationFactory.GetDatasetType(typeName);
-                            var datasetsAsJson = repository
-                                .QueryAsJson(
-                                    $"SELECT * FROM ds WHERE ds.documentType = 'ProviderSourceDataset' AND ds.budgetId = '{budget.Id}' AND ds.datasetName='{typeName}' AND ds.deleted = false")
-                                .ToArray();
-
-                            var typedDatasets = new List<object>();
-
-                            foreach (var json in datasetsAsJson)
-                            {
-                                object sourceRecord =
-                                    JsonConvert.DeserializeObject(json, type, _jsonSerializerSettings);
-
-                                typedDatasets.Add(sourceRecord);
-                            }
-
-                            var StandardFields = new List<string>
-                            {
-                                "Id",
-                                "BudgetId",
-                                "ProviderUrn",
-                                "ProviderName",
-                                "DatasetName"
-                            };
-                            var aggregateFields = new List<AggregateField>();
-                            foreach (var propertyInfo in type.GetProperties()
-                                .Where(x => !StandardFields.Contains(x.Name)))
-                            {
-                                var propertyTypeName = propertyInfo.PropertyType.Name;
-                                switch (propertyTypeName)
-                                {
-                                    case "String":
-
-                                        var strings = typedDatasets.Select(x => propertyInfo.GetValue(x) as string)
-                                            .ToArray();
-
-                                        aggregateFields.Add(new AggregateField
-                                        {
-                                            DatasetDefinitionId = type.Name,
-                                            DatasetFieldDefinitionId = propertyInfo.Name,
-                                            Min = strings.Min(),
-                                            Max = strings.Max(),
-                                            Popular = strings.GroupBy(x => x)
-                                                .Where(x => x.Count() > 1)
-                                                .OrderByDescending(x => x.Count())
-                                                .Select(x => x.Key).Take(10).ToArray()
-                                        });
-                                        break;
-
-                                    case "Decimal":
-
-                                        var decimals = typedDatasets.Select(x => (decimal)propertyInfo.GetValue(x))
-                                            .ToArray();
-
-                                        aggregateFields.Add(new AggregateField
-                                        {
-                                            DatasetDefinitionId = type.Name,
-                                            DatasetFieldDefinitionId = propertyInfo.Name,
-                                            Min = decimals.Min().ToString("0.00"),
-                                            Max = decimals.Max().ToString("0.00"),
-                                            Popular = decimals.GroupBy(x => x)
-                                                .Where(x => x.Count() > 1)
-                                                .OrderByDescending(x => x.Count())
-                                                .Select(x => x.Key.ToString("0.00")).Take(10).ToArray()
-                                        });
-                                        break;
-
-                                    case "Long":
-
-                                        var longs = typedDatasets.Select(x => (long) propertyInfo.GetValue(x))
-                                            .ToArray();
-
-                                        aggregateFields.Add(new AggregateField
-                                        {
-                                            DatasetDefinitionId = type.Name,
-                                            DatasetFieldDefinitionId = propertyInfo.Name,
-                                            Min = longs.Min().ToString(),
-                                            Max = longs.Max().ToString(),
-                                            Popular = longs.GroupBy(x => x)
-                                                .Where(x => x.Count() > 1)
-                                                .OrderByDescending(x => x.Count())
-                                                .Select(x => x.Key.ToString()).Take(10).ToArray()
-                                        });
-                                        break;
-
-
-                                    case "DateTime":
-
-                                        var dateTimes = typedDatasets.Select(x => (DateTime) propertyInfo.GetValue(x))
-                                            .ToArray();
-
-                                        aggregateFields.Add(new AggregateField
-                                        {
-                                            DatasetDefinitionId = type.Name,
-                                            DatasetFieldDefinitionId = propertyInfo.Name,
-                                            Min = dateTimes.Min().ToString("dd/MM/yyyy"),
-                                            Max = dateTimes.Max().ToString("dd/MM/yyyy"),
-                                            Popular = dateTimes.GroupBy(x => x)
-                                                .Where(x => x.Count() > 1)
-                                                .OrderByDescending(x => x.Count())
-                                                .Select(x => x.Key.ToString("dd/MM/yyyy")).Take(10).ToArray()
-                                        });
-                                        break;
-                                }
-                            }
-
-
-                            var aggregateRecord = new AggregateDataset
-                            {
-                                BudgetId = budget.Id,
-                                DatasetName = typeName,
-                                AggregateFields = aggregateFields.ToArray()
-                            };
-                            await aggregateRepository.UpsertAsync(aggregateRecord);
-                        }
-                    }
-                }
-            }
-        }
     }
 
 }
