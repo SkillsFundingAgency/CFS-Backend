b0ef7cd9a5ff6aa91c43e5a13daad3ad0951a9c8
Matt Hammond <matt@talkthinkdo.co.uk>
2017-12-06 06:07:49 +0000

    Change to use resource group name in arm template not unique id

---
+++ DevOps/Merge.sql
@@ -0,0 +1,33 @@
+﻿
+
+MERGE [dbo].[Providers] AS tar
+USING (SELECT * FROM [dbo].[ProviderCommandCandidates] WHERE [ProviderCommandId] = '6516f069-0c3b-4533-aa96-6a23e76c7f84') AS src
+ON tar.[URN] = src.[URN]
+WHEN MATCHED 
+	AND tar.Name <> src.Name
+THEN
+   UPDATE SET
+      tar.Name = src.Name
+WHEN NOT MATCHED THEN
+   INSERT
+   (
+      [URN],
+      [Name],
+	  [CreatedAt],
+	  [UpdatedAt],
+	  [Deleted]
+   )
+   VALUES
+   (
+      src.[URN], 
+	  src.[Name],
+	  src.[CreatedAt],
+	  src.[UpdatedAt],
+	  src.[Deleted]
+   )
+WHEN NOT MATCHED BY SOURCE THEN
+   DELETE
+OUTPUT
+   $action,
+   inserted.*,
+   deleted.*;
