d63a7c7bc0d6150b495932b029b3514667ddad9f
Matt Hammond <matt@talkthinkdo.co.uk>
2017-12-19 13:08:35 +0000

    Update document type

--- CalculateFunding.Models/Command.cs
+++ CalculateFunding.Models/Command.cs
@@ -2,6 +2,10 @@
 {
     public abstract class Command<T> : IIdentifiable where T : IIdentifiable
     {
+        protected Command()
+        {
+            TargetDocumentType = typeof(T).Name;
+        }
         public string Id { get; set; }
         public Reference User { get; set; }
         public string Method { get; set; }
