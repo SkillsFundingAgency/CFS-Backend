c6504cd5ea0ceb4f5ab9ba36568d30f0aad5ba3e
Adam Bryant <adam.bryant@live.co.uk>
2018-01-29 11:08:15 +0000

    Merged PR 2245: Merge ab/fixes/seri-logging to develop
    Related work items: #36952

--- CalculateFunding.Functions.Calcs/Http/Preview.cs
+++ CalculateFunding.Functions.Calcs/Http/Preview.cs
@@ -1,22 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Reflection;
 using System.Threading.Tasks;
-using CalculateFunding.Functions.Calcs.Models;
-using CalculateFunding.Functions.Common;
-using CalculateFunding.Models.Calcs;
-using CalculateFunding.Models.Results;
-using CalculateFunding.Models.Scenarios;
-using CalculateFunding.Repositories.Common.Cosmos;
 using CalculateFunding.Services.Calcs.Interfaces;
-using CalculateFunding.Services.Calculator;
-using CalculateFunding.Services.CodeGeneration;
-using CalculateFunding.Services.CodeGeneration.CSharp;
-using CalculateFunding.Services.CodeGeneration.VisualBasic;
-using CalculateFunding.Services.Compiler;
 using CalculateFunding.Services.Core.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -24,8 +7,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace CalculateFunding.Functions.Calcs.Http
 {
@@ -42,71 +23,5 @@ namespace CalculateFunding.Functions.Calcs.Http
                 return svc.Compile(req);
             }
         }
-
-        [FunctionName("preview")]
-        public static async Task<HttpResponseMessage> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequestMessage req, TraceWriter log, ExecutionContext context)
-        {
-
-            var budgetRepository = ServiceFactory.GetService<CosmosRepository>();
-            
-            var json = await req.Content.ReadAsStringAsync();
-
-            var request = JsonConvert.DeserializeObject<Calcs.Models.PreviewRequest>(json, SerializerSettings);
-            var buildProject = (await budgetRepository.ReadAsync<BuildProject>(request.SpecificationId))?.Content;
-            var calculation = buildProject?.Calculations.FirstOrDefault(x => x.Id == request.CalculationId);
-            if (calculation == null) return new HttpResponseMessage(HttpStatusCode.NotFound);
-
-            if (!string.IsNullOrWhiteSpace(request.SourceCode))
-            {
-                calculation.Current.SourceCode  = request.SourceCode;
-            }
-
-            ISourceFileGenerator generator = null;
-            switch (buildProject.TargetLanguage)
-            {
-                case TargetLanguage.CSharp:
-                    generator = ServiceFactory.GetService<CSharpSourceFileGenerator>();
-                    break;
-                case TargetLanguage.VisualBasic:
-                    generator = ServiceFactory.GetService<VisualBasicSourceFileGenerator>();
-                    break;
-            }
-
-            var sourceFiles = generator.GenerateCode(buildProject);
-
-            var compilerFactory = ServiceFactory.GetService<CompilerFactory>();
-
-            var compiler = compilerFactory.GetCompiler(sourceFiles);
-
-
-            var compilerOutput = compiler.GenerateCode(sourceFiles);
-
-
-            var viewModel = new Models.PreviewResponse()
-            {
-                Calculation = calculation,
-                CompilerOutput = compilerOutput
-            };
-
-
-            return new HttpResponseMessage(HttpStatusCode.OK)
-            {
-                Content = new StringContent(JsonConvert.SerializeObject(viewModel,
-                    SerializerSettings), System.Text.Encoding.UTF8, "application/json")
-            };
-            
-        }
-
-        private static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings
-        {
-            ContractResolver = new CamelCasePropertyNamesContractResolver(),
-            Formatting = Formatting.Indented
-
-        };
-
-
-
-
     }
 }
