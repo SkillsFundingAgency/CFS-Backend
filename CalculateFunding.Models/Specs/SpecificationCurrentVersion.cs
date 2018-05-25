6e582e543156fecf9570dcef010e64a4f24944e5
Daniel Symonds-Lloyd <daniel@wandlesolutions.com>
2018-05-25 11:40:44 +0000

    Merged PR 3452: Current Specification disconnected from Specification Summary
    Related work items: #50501

--- CalculateFunding.Models/Specs/SpecificationCurrentVersion.cs
+++ CalculateFunding.Models/Specs/SpecificationCurrentVersion.cs
@@ -1,13 +1,18 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace CalculateFunding.Models.Specs
 {
-    public class SpecificationCurrentVersion : SpecificationSummary
+    public class SpecificationCurrentVersion : Reference
     {
+        [JsonProperty("fundingPeriod")]
+        public Reference FundingPeriod { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
         [JsonProperty("policies")]
         public IEnumerable<Policy> Policies { get; set; } = Enumerable.Empty<Policy>();
 
@@ -17,6 +22,7 @@ namespace CalculateFunding.Models.Specs
         [JsonProperty("lastUpdatedDate")]
         public DateTime LastUpdatedDate { get; set; }
 
-        public new IEnumerable<FundingStream> FundingStreams { get; set; }
+        [JsonProperty("fundingStreams")]
+        public IEnumerable<FundingStream> FundingStreams { get; set; }
     }
 }
