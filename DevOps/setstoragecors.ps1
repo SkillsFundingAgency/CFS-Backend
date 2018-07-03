89579cdbd99c65f79cf7c6a18ec445d7a8a6cf95
SYMONDS-LLOYD, Daniel <Daniel.SYMONDS-LLOYD@education.gov.uk>
2018-07-03 09:56:48 +0000

    Merged PR 43: Added CORS script

---
+++ DevOps/setstoragecors.ps1
@@ -0,0 +1,34 @@
+﻿param (
+    [Parameter(Mandatory=$true)][string]$storageAccountName,
+    [Parameter(Mandatory=$true)][string]$resourceGroup
+)
+
+Write-Host "Checking CORS rule for storage account '$storageAccountName' in resource group '$resourceGroup'"
+
+$storageAccount = Get-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroup
+
+if($storageAccountName -ne $null)
+{
+    $existingCorsRule = Get-AzureStorageCorsRule -ServiceType Blob -Context $storageAccount.Context
+
+    $CorsRules = (@{
+        AllowedHeaders=@("x-ms-blob-content-type","x-ms-blob-content-disposition", "x-ms-blob-type");
+        AllowedOrigins=@("*");
+        MaxAgeInSeconds=3600;
+        AllowedMethods=@("Get","POST", "PUT")
+        ExposedHeaders=@("x-ms-meta-*", "x-ms-blob-type"); 
+    });
+
+    if($existingCorsRule.Length -eq 0)
+    {
+        Set-AzureStorageCORSRule -ServiceType Blob -CorsRules $CorsRules -Context $storageAccount.Context;
+        Write-Host "Created CORS rule";
+    }
+    else{
+        Write-Host "CORS rule already exists, skipping adding new rule";
+    }
+}
+else
+{
+    Write-Error "Unable to find storage account";
+}
