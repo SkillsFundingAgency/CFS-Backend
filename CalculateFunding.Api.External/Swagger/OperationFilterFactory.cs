94a45a06003f248fbefcb5cba70fd54b3cc560c3
Zulfu Yilmaz <Zulfu.Yilmaz@FASST.org.uk>
2018-07-13 07:04:23 +0100

    Renamed folders + removed comments

--- CalculateFunding.Api.External/Swagger/OperationFilterFactory.cs
+++ CalculateFunding.Api.External/Swagger/OperationFilterFactory.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using CalculateFunding.Api.External.Swagger.Controller.Operations;
 using CalculateFunding.Api.External.Swagger.Operations;
 using CalculateFunding.Api.External.Swagger.Operations.Abstractions;
 using Swashbuckle.AspNetCore.Swagger;
@@ -29,60 +28,5 @@ namespace CalculateFunding.Api.External.Swagger
                 swaggerOperation.Apply(operation, context);
             }
         }
-
-        //public void Apply(Operation operation, OperationFilterContext context)
-        //{
-        //    if (operation.OperationId == "Funding-streamsGet")
-        //    {
-        //        const string etagDescription = "An ETag of the resource";
-        //        const string cacheControlDescription = "Caching information for the resource";
-        //        const string lastModifiedDescription = "Date the resource was last modified";
-
-
-        //        const string acceptHeaderDescription
-        //            = "The calculate funding service uses the Media Type provided in the Accept header to determine " +
-        //              "what representation of a particular resources to serve. " +
-        //              "In particular this includes the version of the resource and the wire format.";
-        //        const string ifNoneMatchDescription
-        //            = "if a previously provided ETag value is provided, the service will return a 304 Not Modified response " +
-        //              "is the resource has not changed.";
-
-        //        operation.Parameters.Add(GenerateHeaderParameter("Accept", true, "string", acceptHeaderDescription));
-        //        operation.Parameters.Add(GenerateHeaderParameter("If-None-Match", false, "string", ifNoneMatchDescription));
-
-        //        operation.Responses.TryGetValue("200", out var response);
-        //        if (response != null)
-        //        {
-        //            var headers = response.Headers ?? new Dictionary<string, Header>();
-        //            headers.Add("ETag", GenerateHeaderResponse(etagDescription, "string"));
-        //            headers.Add("Cache-Control", GenerateHeaderResponse(cacheControlDescription, "string"));
-        //            headers.Add("Last-Modified", GenerateHeaderResponse(lastModifiedDescription, "date"));
-        //            response.Headers = headers;
-        //        }
-        //    }
-        //}
-
-        //public IParameter GenerateHeaderParameter(string name, bool required, string type, string description)
-        //{
-        //    IParameter parameterGenerated = new NonBodyParameter()
-        //    {
-        //        Name = name,
-        //        Required = required,
-        //        Type = type,
-        //        Description = description,
-        //        In = "Header"
-        //    };
-        //    return parameterGenerated;
-        //}
-
-        //public Header GenerateHeaderResponse(string description, string type)
-        //{
-        //    Header headerGenerated = new Header
-        //    {
-        //        Description = "An ETag of the resource",
-        //        Type = type
-        //    };
-        //    return headerGenerated;
-        //}
     }
 }
