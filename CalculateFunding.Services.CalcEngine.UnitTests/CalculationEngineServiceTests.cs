161837587009a6e06322d5591c484f12a255c471
YILMAZ, Zulfu <Zulfu.YILMAZ@education.gov.uk>
2018-07-30 09:15:23 +0000

    Merged PR 111: CalculationEngineServiceTests

---
+++ CalculateFunding.Services.CalcEngine.UnitTests/CalculationEngineServiceTests.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using CalculateFunding.Models.Calcs;
+using CalculateFunding.Models.Results;
+using CalculateFunding.Services.Calculator.Interfaces;
+using CalculateFunding.Services.Core.Interfaces.Caching;
+using CalculateFunding.Services.Core.Interfaces.Logging;
+using CalculateFunding.Services.Core.Interfaces.ServiceBus;
+using CalculateFunding.Services.Core.Options;
+using FluentAssertions;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using NSubstitute;
+using Serilog;
+
+namespace CalculateFunding.Services.Calculator
+{
+    [TestClass]
+    public class CalculationEngineServiceTests
+    {
+        [TestMethod]
+        public void CalculationEngineService_WhenAGivenObjectIsNull_ShouldThrowException()
+        {
+            ILogger mockLogger = Substitute.For<ILogger>();
+            ICalculationEngine mockCalculationService = Substitute.For<ICalculationEngine>();
+            ICacheProvider mockCacheProvider = Substitute.For<ICacheProvider>();
+            IMessengerService mockMessengerService = Substitute.For<IMessengerService>();
+            IProviderSourceDatasetsRepository mockDatasetRepo = Substitute.For<IProviderSourceDatasetsRepository>();
+            ITelemetry mockTelemetry = Substitute.For<ITelemetry>();
+            IProviderResultsRepository mockProviderResultRepo = Substitute.For<IProviderResultsRepository>();
+            ICalculationsRepository mockCalculationRepository = Substitute.For<ICalculationsRepository>();
+            EngineSettings mockEngineSettings = Substitute.For<EngineSettings>();
+
+            Action serviceInstantiator = () =>
+            {
+                CalculationEngineService service =
+                    new CalculationEngineService(
+                        mockLogger,
+                        mockCalculationService,
+                        mockCacheProvider,
+                        mockMessengerService,
+                        mockDatasetRepo,
+                        mockTelemetry,
+                        mockProviderResultRepo,
+                        mockCalculationRepository,
+                        mockEngineSettings,
+                        null);
+            };
+            serviceInstantiator.ShouldThrow<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void GenerateAllocations_WhenMessageDoesNotContainValueForExpectedKey_ShouldThrowException()
+        {
+            // Arrange
+            const string cacheKey = "Cache-key";
+            const string specificationId = "spec1";
+            const int partitionSize = 100;
+
+            CalculationEngineServiceTestsHelper calculationEngineServiceTestsHelper =
+                new CalculationEngineServiceTestsHelper();
+
+            BuildProject buildProject = CreateBuildProject();
+            calculationEngineServiceTestsHelper
+                .MockCalculationRepository
+                .GetBuildProjectBySpecificationId(Arg.Any<string>())
+                .Returns(buildProject);
+
+            CalculationEngineService service = calculationEngineServiceTestsHelper.CreateCalculationEngineService();
+
+            Message message = new Message();
+            IDictionary<string, object> messageUserProperties = message.UserProperties;
+
+            messageUserProperties.Add("provider-summaries-partition-size", partitionSize);
+            messageUserProperties.Add("provider-cache-key", cacheKey);
+            messageUserProperties.Add("specification-id", specificationId);
+
+            // Act
+            Action serviceAction = () => { service.GenerateAllocations(message).Wait(); };
+
+            // Assert
+            serviceAction.ShouldThrow<KeyNotFoundException>();
+        }
+
+        [TestMethod]
+        public void GenerateAllocations_WhenBuildProjectIsNull_ShouldThrowException()
+        {
+            // Arrange
+            const string cacheKey = "Cache-key";
+            const string specificationId = "spec1";
+            const int partitionIndex = 0;
+            const int partitionSize = 100;
+
+            CalculationEngineServiceTestsHelper calculationEngineServiceTestsHelper =
+                new CalculationEngineServiceTestsHelper();
+
+            CalculationEngineService service = calculationEngineServiceTestsHelper.CreateCalculationEngineService();
+
+            Message message = new Message();
+            IDictionary<string, object> messageUserProperties = message.UserProperties;
+
+            messageUserProperties.Add("provider-summaries-partition-index", partitionIndex);
+            messageUserProperties.Add("provider-summaries-partition-size", partitionSize);
+            messageUserProperties.Add("provider-cache-key", cacheKey);
+            messageUserProperties.Add("specification-id", specificationId);
+
+            // Act
+            Action serviceAction = () => { service.GenerateAllocations(message).Wait(); };
+
+            // Assert
+            serviceAction.ShouldThrow<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void GenerateAllocations_WhenPartitionSizeIsZero_ShouldThrowException()
+        {
+            // Arrange
+            const string cacheKey = "Cache-key";
+            const string specificationId = "spec1";
+            const int partitionIndex = 0;
+            const int partitionSize = 0;
+
+            CalculationEngineServiceTestsHelper calculationEngineServiceTestsHelper =
+                new CalculationEngineServiceTestsHelper();
+            BuildProject buildProject = CreateBuildProject();
+            calculationEngineServiceTestsHelper
+                .MockCalculationRepository
+                .GetBuildProjectBySpecificationId(Arg.Any<string>())
+                .Returns(buildProject);
+
+            CalculationEngineService service = calculationEngineServiceTestsHelper.CreateCalculationEngineService();
+
+            Message message = new Message();
+            IDictionary<string, object> messageUserProperties = message.UserProperties;
+
+            messageUserProperties.Add("provider-summaries-partition-index", partitionIndex);
+            messageUserProperties.Add("provider-summaries-partition-size", partitionSize);
+            messageUserProperties.Add("provider-cache-key", cacheKey);
+            messageUserProperties.Add("specification-id", specificationId);
+
+            // Act
+            Action serviceAction = () => { service.GenerateAllocations(message).Wait(); };
+
+            // Assert
+            serviceAction.ShouldThrow<KeyNotFoundException>().And.Message
+                .ShouldBeEquivalentTo($"Partition size is zero or less. {partitionSize}");
+        }
+
+        [TestMethod]
+        public void
+            GenerateAllocations_GivenAValidRequestWhereSaveProviderResultsNotIgnored_ShouldBatchCorrectlyAndSaveProviderResults()
+        {
+            const string cacheKey = "Cache-key";
+            const string specificationId = "spec1";
+            const int partitionIndex = 0;
+            const int partitionSize = 100;
+            const int stop = partitionIndex + partitionSize - 1;
+
+            CalculationEngineServiceTestsHelper calculationEngineServiceTestsHelper =
+                new CalculationEngineServiceTestsHelper();
+
+            BuildProject buildProject = CreateBuildProject();
+
+            IList<ProviderSummary> providerSummaries = MockData.GetDummyProviders(20);
+
+            IAllocationModel mockAllocationModel = Substitute.For<IAllocationModel>();
+            mockAllocationModel
+                .Execute(Arg.Any<List<ProviderSourceDatasetCurrent>>())
+                .Returns(new List<CalculationResult>());
+
+            calculationEngineServiceTestsHelper
+                .MockCacheProvider
+                .ListRangeAsync<ProviderSummary>(cacheKey, partitionIndex, stop)
+                .Returns(providerSummaries);
+
+            calculationEngineServiceTestsHelper
+                .MockCalculationRepository
+                .GetBuildProjectBySpecificationId(Arg.Any<string>())
+                .Returns(buildProject);
+
+            IList<CalculationSummaryModel> calculationSummaryModelsReturn = CreateDummyCalculationSummaryModels();
+            calculationEngineServiceTestsHelper
+                .MockCalculationRepository
+                .GetCalculationSummariesForSpecification(specificationId)
+                .Returns(calculationSummaryModelsReturn);
+
+            calculationEngineServiceTestsHelper
+                .MockCalculationEngine
+                .GenerateAllocationModel(Arg.Any<BuildProject>())
+                .Returns(mockAllocationModel);
+            calculationEngineServiceTestsHelper
+                .MockCalculationEngine
+                .CalculateProviderResults(mockAllocationModel, buildProject, calculationSummaryModelsReturn,
+                    Arg.Is<ProviderSummary>(summary => providerSummaries.Contains(summary)),
+                    Arg.Any<IEnumerable<ProviderSourceDatasetCurrent>>())
+                .Returns((ProviderResult) null);
+
+            calculationEngineServiceTestsHelper
+                .MockEngineSettings
+                .ProviderBatchSize = 3;
+
+            CalculationEngineService service = calculationEngineServiceTestsHelper.CreateCalculationEngineService();
+
+            Message message = new Message();
+            IDictionary<string, object> messageUserProperties = message.UserProperties;
+
+            messageUserProperties.Add("provider-summaries-partition-index", partitionIndex);
+            messageUserProperties.Add("provider-summaries-partition-size", partitionSize);
+            messageUserProperties.Add("provider-cache-key", cacheKey);
+            messageUserProperties.Add("specification-id", specificationId);
+
+            service.GenerateAllocations(message).Wait();
+
+            calculationEngineServiceTestsHelper
+                .MockProviderResultRepo
+                .Received(0)
+                .SaveProviderResults(Arg.Any<IEnumerable<ProviderResult>>(), Arg.Any<int>());
+        }
+
+        [TestMethod]
+        public void GenerateAllocations_GivenAValidRequestWhereNoResultsWereReturned_ShouldNotSaveAnything()
+        {
+            const string cacheKey = "Cache-key";
+            const string specificationId = "spec1";
+            const int partitionIndex = 0;
+            const int partitionSize = 100;
+            const int stop = partitionIndex + partitionSize - 1;
+
+            CalculationEngineServiceTestsHelper calculationEngineServiceTestsHelper =
+                new CalculationEngineServiceTestsHelper();
+
+            BuildProject buildProject = CreateBuildProject();
+
+            IList<ProviderSummary> providerSummaries = MockData.GetDummyProviders(20);
+
+            IAllocationModel mockAllocationModel = Substitute.For<IAllocationModel>();
+            mockAllocationModel
+                .Execute(Arg.Any<List<ProviderSourceDatasetCurrent>>())
+                .Returns(new List<CalculationResult>());
+
+            calculationEngineServiceTestsHelper
+                .MockCacheProvider
+                .ListRangeAsync<ProviderSummary>(cacheKey, partitionIndex, stop)
+                .Returns(providerSummaries);
+
+            calculationEngineServiceTestsHelper
+                .MockCalculationRepository
+                .GetBuildProjectBySpecificationId(Arg.Any<string>())
+                .Returns(buildProject);
+
+            IList<CalculationSummaryModel> calculationSummaryModelsReturn = CreateDummyCalculationSummaryModels();
+            calculationEngineServiceTestsHelper
+                .MockCalculationRepository
+                .GetCalculationSummariesForSpecification(specificationId)
+                .Returns(calculationSummaryModelsReturn);
+
+            calculationEngineServiceTestsHelper
+                .MockCalculationEngine
+                .GenerateAllocationModel(Arg.Any<BuildProject>())
+                .Returns(mockAllocationModel);
+            calculationEngineServiceTestsHelper
+                .MockCalculationEngine
+                .CalculateProviderResults(mockAllocationModel, buildProject, calculationSummaryModelsReturn,
+                    Arg.Is<ProviderSummary>(summary => providerSummaries.Contains(summary)),
+                    Arg.Any<IEnumerable<ProviderSourceDatasetCurrent>>())
+                .Returns(new ProviderResult()
+                {
+
+                });
+
+            calculationEngineServiceTestsHelper
+                .MockEngineSettings
+                .ProviderBatchSize = 3;
+
+            CalculationEngineService service = calculationEngineServiceTestsHelper.CreateCalculationEngineService();
+
+            Message message = new Message();
+            IDictionary<string, object> messageUserProperties = message.UserProperties;
+
+            messageUserProperties.Add("provider-summaries-partition-index", partitionIndex);
+            messageUserProperties.Add("provider-summaries-partition-size", partitionSize);
+            messageUserProperties.Add("provider-cache-key", cacheKey);
+            messageUserProperties.Add("specification-id", specificationId);
+
+            service.GenerateAllocations(message).Wait();
+
+            calculationEngineServiceTestsHelper
+                .MockCalculationEngine
+                .Received(providerSummaries.Count)
+                .CalculateProviderResults(mockAllocationModel, buildProject, calculationSummaryModelsReturn,
+                    Arg.Any<ProviderSummary>(), Arg.Any<IEnumerable<ProviderSourceDatasetCurrent>>());
+
+            calculationEngineServiceTestsHelper
+                .MockProviderResultRepo
+                .Received(7)
+                .SaveProviderResults(Arg.Any<IEnumerable<ProviderResult>>(), Arg.Any<int>());
+        }
+
+        [TestMethod]
+        public void
+            GenerateAllocations_GivenAValidRequestWhereIgnoreSaveProviderResultsFlagIsSet_ShouldNotSaveProviderResults()
+        {
+            const string cacheKey = "Cache-key";
+            const string specificationId = "spec1";
+            const int partitionIndex = 0;
+            const int partitionSize = 100;
+            const int stop = partitionIndex + partitionSize - 1;
+
+            CalculationEngineServiceTestsHelper calculationEngineServiceTestsHelper =
+                new CalculationEngineServiceTestsHelper();
+
+            BuildProject buildProject = CreateBuildProject();
+
+            IList<ProviderSummary> providerSummaries = MockData.GetDummyProviders(20);
+
+            IAllocationModel mockAllocationModel = Substitute.For<IAllocationModel>();
+            mockAllocationModel
+                .Execute(Arg.Any<List<ProviderSourceDatasetCurrent>>())
+                .Returns(new List<CalculationResult>());
+
+            calculationEngineServiceTestsHelper
+                .MockCacheProvider
+                .ListRangeAsync<ProviderSummary>(cacheKey, partitionIndex, stop)
+                .Returns(providerSummaries);
+
+            calculationEngineServiceTestsHelper
+                .MockCalculationRepository
+                .GetBuildProjectBySpecificationId(Arg.Any<string>())
+                .Returns(buildProject);
+
+            IList<CalculationSummaryModel> calculationSummaryModelsReturn = CreateDummyCalculationSummaryModels();
+            calculationEngineServiceTestsHelper
+                .MockCalculationRepository
+                .GetCalculationSummariesForSpecification(specificationId)
+                .Returns(calculationSummaryModelsReturn);
+
+            calculationEngineServiceTestsHelper
+                .MockCalculationEngine
+                .GenerateAllocationModel(Arg.Any<BuildProject>())
+                .Returns(mockAllocationModel);
+            calculationEngineServiceTestsHelper
+                .MockCalculationEngine
+                .CalculateProviderResults(mockAllocationModel, buildProject, calculationSummaryModelsReturn,
+                    Arg.Is<ProviderSummary>(summary => providerSummaries.Contains(summary)),
+                    Arg.Any<IEnumerable<ProviderSourceDatasetCurrent>>())
+                .Returns(new ProviderResult()
+                {
+
+                });
+
+            calculationEngineServiceTestsHelper
+                .MockEngineSettings
+                .ProviderBatchSize = 3;
+
+            CalculationEngineService service = calculationEngineServiceTestsHelper.CreateCalculationEngineService();
+
+            Message message = new Message();
+            IDictionary<string, object> messageUserProperties = message.UserProperties;
+
+            messageUserProperties.Add("provider-summaries-partition-index", partitionIndex);
+            messageUserProperties.Add("provider-summaries-partition-size", partitionSize);
+            messageUserProperties.Add("provider-cache-key", cacheKey);
+            messageUserProperties.Add("specification-id", specificationId);
+            messageUserProperties.Add("ignore-save-provider-results", "true");
+
+            service.GenerateAllocations(message).Wait();
+
+            calculationEngineServiceTestsHelper
+                .MockCalculationEngine
+                .Received(providerSummaries.Count)
+                .CalculateProviderResults(mockAllocationModel, buildProject, calculationSummaryModelsReturn,
+                    Arg.Any<ProviderSummary>(), Arg.Any<IEnumerable<ProviderSourceDatasetCurrent>>());
+
+            calculationEngineServiceTestsHelper
+                .MockProviderResultRepo
+                .Received(0)
+                .SaveProviderResults(Arg.Any<IEnumerable<ProviderResult>>(), Arg.Any<int>());
+        }
+
+        private static BuildProject CreateBuildProject()
+        {
+            BuildProject buildProject = JsonConvert.DeserializeObject<BuildProject>(MockData.SerializedBuildProject());
+
+            return buildProject;
+        }
+
+        private static IList<CalculationSummaryModel> CreateDummyCalculationSummaryModels()
+        {
+            List<CalculationSummaryModel> calculationSummaryModels = new List<CalculationSummaryModel>()
+            {
+                new CalculationSummaryModel()
+                {
+                    Name = "TestCalc1",
+                    CalculationType = CalculationType.Funding,
+                    Id = "TC1"
+                },
+                new CalculationSummaryModel()
+                {
+                    Name = "TestCalc2",
+                    CalculationType = CalculationType.Number,
+                    Id = "TC2"
+                }
+            };
+            return calculationSummaryModels;
+        }
+    }
+}
---
+++ CalculateFunding.Services.CalcEngine.UnitTests/CalculationEngineServiceTestsHelper.cs
@@ -0,0 +1,57 @@
+﻿using CalculateFunding.Services.Calculator.Interfaces;
+using CalculateFunding.Services.Core.Interfaces.Caching;
+using CalculateFunding.Services.Core.Interfaces.Logging;
+using CalculateFunding.Services.Core.Interfaces.ServiceBus;
+using CalculateFunding.Services.Core.Options;
+using NSubstitute;
+using Polly;
+using Serilog;
+
+namespace CalculateFunding.Services.Calculator
+{
+    public class CalculationEngineServiceTestsHelper
+    {
+        public CalculationEngineServiceTestsHelper()
+        {
+            MockCalculatorResiliencePolicies.CacheProvider.Returns(MockCacheProviderPolicy);
+            MockCalculatorResiliencePolicies.Messenger.Returns(MockMessengerPolicy);
+            MockCalculatorResiliencePolicies.ProviderSourceDatasetsRepository.Returns(MockProviderSourceDatasetsRepositoryPolicy);
+            MockCalculatorResiliencePolicies.ProviderResultsRepository.Returns(MockProviderResultsRepositoryPolicy);
+            MockCalculatorResiliencePolicies.CalculationsRepository.Returns(MockCalculationRepositoryPolicy);
+        }
+
+        public CalculationEngineService CreateCalculationEngineService()
+        {
+            CalculationEngineService service =
+                new CalculationEngineService(
+                    MockLogger,
+                    MockCalculationEngine,
+                    MockCacheProvider,
+                    MockMessengerService,
+                    MockDatasetRepo,
+                    MockTelemetry,
+                    MockProviderResultRepo,
+                    MockCalculationRepository,
+                    MockEngineSettings,
+                    MockCalculatorResiliencePolicies);
+
+            return service;
+        }
+
+        public ILogger MockLogger { get; set; } = Substitute.For<ILogger>();
+        public ICacheProvider MockCacheProvider { get; set; } = Substitute.For<ICacheProvider>();
+        public IMessengerService MockMessengerService { get; set; } = Substitute.For<IMessengerService>();
+        public IProviderSourceDatasetsRepository MockDatasetRepo { get; set; } = Substitute.For<IProviderSourceDatasetsRepository>();
+        public ITelemetry MockTelemetry { get; set; } = Substitute.For<ITelemetry>();
+        public IProviderResultsRepository MockProviderResultRepo { get; set; } = Substitute.For<IProviderResultsRepository>();
+        public ICalculationsRepository MockCalculationRepository { get; set; } = Substitute.For<ICalculationsRepository>();
+        public EngineSettings MockEngineSettings { get; set; } = Substitute.For<EngineSettings>();
+        public ICalculatorResiliencePolicies MockCalculatorResiliencePolicies { get; set; } = Substitute.For<ICalculatorResiliencePolicies>();
+        public ICalculationEngine MockCalculationEngine { get; set; } = Substitute.For<ICalculationEngine>();
+        public Policy MockCacheProviderPolicy { get; set; } = Policy.NoOpAsync();
+        public Policy MockMessengerPolicy { get; set; } = Policy.NoOpAsync();
+        public Policy MockProviderSourceDatasetsRepositoryPolicy { get; set; } = Policy.NoOpAsync();
+        public Policy MockProviderResultsRepositoryPolicy { get; set; } = Policy.NoOpAsync();
+        public Policy MockCalculationRepositoryPolicy { get; set; } = Policy.NoOpAsync();
+    }
+}
