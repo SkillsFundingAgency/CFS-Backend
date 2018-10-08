17c2e4adbf0e5b76706c3087ee73daf8b07dd8da
Ravi Thippeswamy <ravi.thippeswamy@education.gov.uk>
2018-10-08 18:27:24 +0100

    Changes to dataset service and metadata model to process special characters while creating dataset

--- CalculateFunding.Models/Datasets/DatasetMetadataModel.cs
+++ CalculateFunding.Models/Datasets/DatasetMetadataModel.cs
@@ -6,20 +6,6 @@ namespace CalculateFunding.Models.Datasets
     public class DatasetMetadataModel
     {
         public DatasetMetadataModel(){}
-
-        public DatasetMetadataModel(IDictionary<string, string> metadData)
-        {
-            if (metadData == null)
-                throw new ArgumentNullException(nameof(metadData));
-
-            AuthorName = metadData.ContainsKey("authorName") ? metadData["authorName"] : string.Empty;
-            AuthorId = metadData.ContainsKey("authorId") ? metadData["authorId"] : string.Empty;
-            DatasetId = metadData.ContainsKey("datasetId") ? metadData["datasetId"] : string.Empty;
-            DataDefinitionId = metadData.ContainsKey("dataDefinitionId") ? metadData["dataDefinitionId"] : string.Empty;
-            Name = metadData.ContainsKey("name") ? metadData["name"] : string.Empty;
-            Description = metadData.ContainsKey("description") ? metadData["description"] : string.Empty;
-        }
-
         public string AuthorName { get; set; }
         public string AuthorId { get; set; }
         public string DatasetId { get; set; }
