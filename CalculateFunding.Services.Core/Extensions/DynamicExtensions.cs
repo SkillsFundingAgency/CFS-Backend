193fbb34254f063a9dd381fbadbe361c8e93cafe
BRYANT, Adam <Adam.BRYANT@education.gov.uk>
2018-10-22 16:34:51 +0000

    Merged PR 479: Check for null in dynamic

---
+++ CalculateFunding.Services.Core/Extensions/DynamicExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculateFunding.Services.Core.Extensions
+{
+    public static class DynamicExtensions
+    {
+        public static bool PropertyExists(dynamic obj, string name)
+        {
+             return ((IDictionary<string, object>)obj).ContainsKey(name);
+        }
+    }
+}
