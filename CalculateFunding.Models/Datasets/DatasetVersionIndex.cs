f3a5db0e7242da99eb660cd58a4c78b0d3b8e37a
YILMAZ, Zulfu <Zulfu.YILMAZ@education.gov.uk>
2019-02-11 10:42:56 +0000

    Merged PR 1805: Createed new datasetversionindex for version page history page
    Related work items: #8495

---
+++ CalculateFunding.Models/Datasets/DatasetVersionIndex.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.Search;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CalculateFunding.Models.Datasets
+{
+	[SearchIndex(IndexerForType = typeof(Dataset),
+		CollectionName = "results",
+		DatabaseName = "allocations")]
+	public class DatasetVersionIndex
+	{
+		[Key]
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[IsFilterable]
+		[JsonProperty("datasetId")]
+		public string DatasetId { get; set; }
+		
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("description")]
+		public string Description { get; set; }
+
+		[JsonProperty("changeNote")]
+		public string ChangeNote { get; set; }
+		
+		[JsonProperty("version"), IsSortable]
+		public int Version { get; set; }
+
+		[JsonProperty("definitionName")]
+		public string DefinitionName { get; set; }
+
+		[IsSortable]
+		[JsonProperty("lastUpdatedDate")]
+		public DateTimeOffset LastUpdatedDate { get; set; }
+
+		[JsonProperty("lastUpdatedByName")]
+		public string LastUpdatedByName { get; set; }
+
+		[JsonProperty("blobName")]
+		public string BlobName { get; set; }
+	}
+}
