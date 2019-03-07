0af2ee7160905cc27d92a0008f20a165e23201ca
Dan Symonds-Lloyd <Daniel.SYMONDS-LLOYD@education.gov.uk>
2019-03-07 14:32:39 +0000

    Merged PR 2138: Fix for dataset version index
    Fix for dataset version index
    
    Related work items: #12836

--- CalculateFunding.Models/Datasets/DatasetVersionIndex.cs
+++ CalculateFunding.Models/Datasets/DatasetVersionIndex.cs
@@ -1,46 +1,48 @@
-﻿using Microsoft.Azure.Search;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.Azure.Search;
+using Newtonsoft.Json;
 
 namespace CalculateFunding.Models.Datasets
 {
-	[SearchIndex(IndexerForType = typeof(Dataset),
-		CollectionName = "results",
-		DatabaseName = "allocations")]
-	public class DatasetVersionIndex
-	{
-		[Key]
-		[JsonProperty("id")]
-		public string Id { get; set; }
+    [SearchIndex(IndexerForType = typeof(Dataset),
+        CollectionName = "results",
+        DatabaseName = "allocations")]
+    public class DatasetVersionIndex
+    {
+        [Key]
+        [JsonProperty("id")]
+        public string Id { get; set; }
 
-		[IsFilterable]
-		[JsonProperty("datasetId")]
-		public string DatasetId { get; set; }
-		
-		[JsonProperty("name")]
-		public string Name { get; set; }
+        [IsFilterable]
+        [JsonProperty("datasetId")]
+        public string DatasetId { get; set; }
 
-		[JsonProperty("description")]
-		public string Description { get; set; }
+        [JsonProperty("name")]
+        [IsSearchable, IsSortable]
+        public string Name { get; set; }
 
-		[JsonProperty("changeNote")]
-		public string ChangeNote { get; set; }
-		
-		[JsonProperty("version"), IsSortable]
-		public int Version { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
 
-		[JsonProperty("definitionName")]
-		public string DefinitionName { get; set; }
+        [JsonProperty("changeNote")]
+        public string ChangeNote { get; set; }
 
-		[IsSortable]
-		[JsonProperty("lastUpdatedDate")]
-		public DateTimeOffset LastUpdatedDate { get; set; }
+        [JsonProperty("version"), IsSortable]
+        public int Version { get; set; }
 
-		[JsonProperty("lastUpdatedByName")]
-		public string LastUpdatedByName { get; set; }
+        [JsonProperty("definitionName")]
+        [IsFilterable, IsSortable]
+        public string DefinitionName { get; set; }
 
-		[JsonProperty("blobName")]
-		public string BlobName { get; set; }
-	}
+        [IsSortable]
+        [JsonProperty("lastUpdatedDate")]
+        public DateTimeOffset LastUpdatedDate { get; set; }
+
+        [JsonProperty("lastUpdatedByName")]
+        public string LastUpdatedByName { get; set; }
+
+        [JsonProperty("blobName")]
+        public string BlobName { get; set; }
+    }
 }
