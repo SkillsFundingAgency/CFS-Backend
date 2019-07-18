d66d703b31b79ed3924c84d1164b115eb5b33e7b
TATE, Matthew <Matthew.TATE@education.gov.uk>
2019-07-18 14:45:58 +0000

    Merged PR 3901: Added missing provider models.
    Added missing provider models.
    
    Related work items: #23486

---
+++ CalculateFunding.Models/Publishing/PublishedProviderStatus.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CalculateFunding.Models.Publishing
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum PublishedProviderStatus
+    {
+        Held,
+        Approved,
+        Released,
+        Updated
+    }
+}
---
+++ CalculateFunding.Models/Publishing/PublishedProviderVersion.cs
@@ -0,0 +1,48 @@
+﻿using CalculateFunding.Models.Versioning;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculateFunding.Models.Publishing
+{
+    public class PublishedProviderVersion : VersionedItem
+    {
+        [JsonProperty("id")]
+        public override string Id => $"publishedprovider_{ProviderId}_{FundingPeriodId}_{FundingStreamId}_{Version}";
+
+        [JsonProperty("fundingStreamId")]
+        public string FundingStreamId { get; set; }
+
+        [JsonProperty("fundingPeriodId")]
+        public string FundingPeriodId { get; set; }
+
+        [JsonProperty("feedIndexId")]
+        public string FeedIndexId { get; set; }
+
+        [JsonProperty("specificationId")]
+        public string SpecificationId { get; set; }
+
+        [JsonProperty("entityId")]
+        public override string EntityId
+        {
+            get { return $"publishedprovider_{ProviderId}_{FundingPeriodId}_{FundingStreamId}"; }
+        }
+
+        [JsonProperty("status")]
+        public PublishedProviderStatus Status { get; set; }
+
+        [JsonProperty("providerId")]
+        public string ProviderId { get; set; }
+
+        [JsonProperty("partitionKey")]
+        public string PartitionKey => $"publishedprovider_{ProviderId}_{FundingPeriodId}_{FundingStreamId}";
+
+        public override VersionedItem Clone()
+        {
+            // Serialise to perform a deep copy
+            string json = JsonConvert.SerializeObject(this);
+            return JsonConvert.DeserializeObject<PublishedProviderVersion>(json);
+        }
+    }
+}
