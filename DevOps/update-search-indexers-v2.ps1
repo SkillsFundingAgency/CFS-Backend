e392c609caf6c83fad67c42c78727b5867ea3589
Mike SPINKS <Mike.SPINKS@EDUCATION.GOV.UK>
2019-09-03 12:57:21 +0000

    Merged PR 4314: Edit of Powershell and ARM Templates for v2
    I had to edit the powershell scripts and ARM Templates so they referred to the new productVersion.
    
    So as not to conflict with the current pipeline I appended -v2 or v2. to the files and updated the Task Groups / Release pipelines.
    
    Related work items: #18072

---
+++ DevOps/update-search-indexers-v2.ps1
@@ -0,0 +1,63 @@
+﻿param(
+    [Parameter(Position = 0, mandatory = $true)]
+    [string]$apiKey,
+
+    [Parameter(Position = 1, mandatory = $true)]
+    [string]$environmentKey,
+    
+    [Parameter(Position = 2, mandatory = $true)]
+    [String[]] $indexers = @(),
+
+    [Parameter(Position = 3, mandatory = $true)]
+    [string]$productVersion
+)
+$apiKeyObj = $apiKey | convertfrom-json
+
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+
+$rootFolder = "$ScriptDir\search-indexers"
+
+Write-Host "Updating indexers on: $searchServiceUrl";
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]";
+$headers.Add("api-key", $apiKeyObj.azureSearchKey.value);
+
+$indexers | ForEach-Object {
+    
+    $schema = Get-Content -Raw -Path "$rootFolder\$_\$_.json" | ConvertFrom-Json
+
+    $indexerName = $schema.name
+
+    $schema = $schema | ConvertTo-Json
+
+    $searchServiceUrl = "https://ss-$environmentKey-cfs-$productVersion.search.windows.net/indexers/$($indexerName)?api-version=2019-05-06"
+
+    $stopTrying = $false
+
+     do {
+            try {
+                $responseData = Invoke-WebRequest -Uri $searchServiceUrl -Method PUT -Headers $headers -Body $schema -ContentType "application/json; charset=utf-8" -DisableKeepAlive;
+                Write-Host "$_ saved successfully" -ForegroundColor Green
+                $stopTrying = $true
+            }
+            catch {
+                $responseData = $_.Exception
+
+                if ($retrycount -gt 3) {
+                    Write-Host "##vso[task.logissue type=error;] $_ failed after 3 attempts" -ForegroundColor Red
+                    Write-Error $responseData
+                    $stopTrying = $true
+                }
+                else {
+                    Write-Host "##vso[task.logissue type=warning;] $_ failed, retrying in 3 seconds." -ForegroundColor Yellow
+                    Start-Sleep -Seconds 3
+                    $retrycount = $retrycount + 1
+                }
+
+            }
+        } 
+        While ($stopTrying -eq $false)
+
+}
+
+
