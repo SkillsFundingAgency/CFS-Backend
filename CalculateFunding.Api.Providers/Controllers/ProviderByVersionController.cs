201fdba8b8fadde5ae2dbda8375d08323b8de231
TATE, Matthew <Matthew.TATE@education.gov.uk>
2020-09-22 08:40:32 +0000

    Merged PR 8996: Increase content length limit.
    Increase content length limit.
    
    Related work items: #52809

--- CalculateFunding.Api.Providers/Controllers/ProviderByVersionController.cs
+++ CalculateFunding.Api.Providers/Controllers/ProviderByVersionController.cs
@@ -114,7 +114,7 @@ namespace CalculateFunding.Api.Providers.Controllers
         /// <param name="providers">List of Providers</param>
         /// <returns></returns>
         [HttpPost("api/providers/versions/{providerVersionId}")]
-        [DisableRequestSizeLimitAttribute()]
+        [DisableRequestSizeLimit()]
         public async Task<IActionResult> UploadProviderVersion([FromRoute]string providerVersionId, [FromBody]ProviderVersionViewModel providers)
         {
             string controllerName = string.Empty;
