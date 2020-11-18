af31463bd7575ea6bb33fa366aa71a6cf7b7bb05
TATE, Matthew <Matthew.TATE@education.gov.uk>
2020-11-18 11:16:42 +0000

    Merged PR 9676: When running locally the deadletter messages are not being processed.
    When running locally the deadletter messages are not being processed.
    
    Related work items: #55179

--- CalculateFunding.Services.Core/Constants/ServiceBusConstants.cs
+++ CalculateFunding.Services.Core/Constants/ServiceBusConstants.cs
@@ -10,25 +10,25 @@
 
             public const string ApplyTemplateCalculationsPoisoned = "apply-template-calculations/$DeadLetterQueue";
 
-            public const string ApplyTemplateCalculationsPoisonedLocal = "apply-template-calculations-poisoned";
+            public const string ApplyTemplateCalculationsPoisonedLocal = "apply-template-calculations-poison";
 
             public const string ApproveAllCalculations = "calculations-approve-all";
 
             public const string ApproveAllCalculationsPoisoned = "calculations-approve-all/$DeadLetterQueue";
 
-            public const string ApproveAllCalculationsPoisonedLocal = "calculations-approve-all-poisoned";
+            public const string ApproveAllCalculationsPoisonedLocal = "calculations-approve-all-poison";
 
             public const string CalculationJobInitialiser = "calc-events-instruct-generate-allocations";
 
             public const string CalculationJobInitialiserPoisoned = "calc-events-instruct-generate-allocations/$DeadLetterQueue";
 
-            public const string CalculationJobInitialiserPoisonedLocal = "calc-events-instruct-generate-allocations-poisoned";
+            public const string CalculationJobInitialiserPoisonedLocal = "calc-events-instruct-generate-allocations-poison";
 
             public const string CalcEngineGenerateAllocationResults = "calc-events-generate-allocations-results";
 
             public const string CalcEngineGenerateAllocationResultsPoisoned = "calc-events-generate-allocations-results/$DeadLetterQueue";
 
-            public const string CalcEngineGenerateAllocationResultsPoisonedLocal = "calc-events-generate-allocations-results-poisoned";
+            public const string CalcEngineGenerateAllocationResultsPoisonedLocal = "calc-events-generate-allocations-results-poison";
 
             public const string TestEngineExecuteTests = "test-events-execute-tests";
 
@@ -40,17 +40,17 @@
 
             public const string MapFdzDatasetsPoisoned = "map-fdz-datasets/$DeadLetterQueue";
 
-            public const string MapFdzDatasetsPoisonedLocal = "map-fdz-datasets-poisoned";
+            public const string MapFdzDatasetsPoisonedLocal = "map-fdz-datasets-poison";
 
             public const string ProcessDatasetPoisoned = "dataset-events-datasets/$DeadLetterQueue";
 
-            public const string ProcessDatasetPoisonedLocal = "dataset-events-datasets-poisoned";
+            public const string ProcessDatasetPoisonedLocal = "dataset-events-datasets-poison";
 
             public const string ValidateDataset = "dataset-validate";
 
             public const string ValidateDatasetPoisoned = "dataset-validate/$DeadLetterQueue";
 
-            public const string ValidateDatasetPoisonedLocal = "dataset-validate-poisoned";
+            public const string ValidateDatasetPoisonedLocal = "dataset-validate-poison";
 
             public const string CreateDraftCalculation = "calc-events-create-draft";
 
@@ -62,35 +62,35 @@
 
             public const string PublishIntegrityCheckPoisoned = "publish-integrity-check/$DeadLetterQueue";
 
-            public const string PublishIntegrityCheckPoisonedLocal = "publish-integrity-check-poisoned";
+            public const string PublishIntegrityCheckPoisonedLocal = "publish-integrity-check-poison";
 
             public const string PublishingApproveAllProviderFundingPoisoned = "publishing-approve-all-provider-funding/$DeadLetterQueue";
 
-            public const string PublishingApproveAllProviderFundingPoisonedLocal = "publishing-approve-all-provider-funding-poisoned";
+            public const string PublishingApproveAllProviderFundingPoisonedLocal = "publishing-approve-all-provider-funding-poison";
 
             public const string PublishingApproveBatchProviderFunding = "publishing-approve-batch-provider-funding";
                                                  
             public const string PublishingApproveBatchProviderFundingPoisoned = "publishing-approve-batch-provider-funding/$DeadLetterQueue";
 
-            public const string PublishingApproveBatchProviderFundingPoisonedLocal = "publishing-approve-batch-provider-funding-poisoned";
+            public const string PublishingApproveBatchProviderFundingPoisonedLocal = "publishing-approve-batch-provider-funding-poison";
 
             public const string PublishingRefreshFunding = "publishing-refreshfunding";
 
             public const string PublishingRefreshFundingPoisoned = "publishing-refreshfunding/$DeadLetterQueue";
 
-            public const string PublishingRefreshFundingPoisonedLocal = "publishing-refreshfunding-poisoned";
+            public const string PublishingRefreshFundingPoisonedLocal = "publishing-refreshfunding-poison";
 
             public const string PublishingPublishAllProviderFunding = "publishing-publish-all-provider-funding";
 
             public const string PublishingPublishAllProviderFundingPoisoned = "publishing-publish-all-provider-funding/$DeadLetterQueue";
 
-            public const string PublishingPublishAllProviderFundingPoisonedLocal = "publishing-publish-all-provider-funding-poisoned";
+            public const string PublishingPublishAllProviderFundingPoisonedLocal = "publishing-publish-all-provider-funding-poison";
 
             public const string PublishingPublishBatchProviderFunding = "publishing-publish-batch-provider-funding";
 
             public const string PublishingPublishBatchProviderFundingPoisoned = "publishing-publish-batch-provider-funding/$DeadLetterQueue";
 
-            public const string PublishingPublishBatchProviderFundingPoisonedLocal = "publishing-publish-batch-provider-funding-poisoned";
+            public const string PublishingPublishBatchProviderFundingPoisonedLocal = "publishing-publish-batch-provider-funding-poison";
 
             public const string ReIndexCalculationResultsIndex = "reindex-calculation-results-index";
 
@@ -107,49 +107,49 @@
             
             public const string PublishingReIndexPublishedProvidersPoisoned = "publishing-reindex-published-providers/$DeadLetterQueue";
             
-            public const string PublishingReIndexPublishedProvidersPoisonedLocal = "publishing-reindex-published-providers-poisoned";
+            public const string PublishingReIndexPublishedProvidersPoisonedLocal = "publishing-reindex-published-providers-poison";
 
             public const string PopulateScopedProviders = "populate-scopedproviders";
 
             public const string PopulateScopedProvidersPoisoned = "populate-scopedproviders/$DeadLetterQueue";
 
-            public const string PopulateScopedProvidersPoisonedLocal = "populate-scopedproviders-poisoned";
+            public const string PopulateScopedProvidersPoisonedLocal = "populate-scopedproviders-poison";
 
             public const string DeleteCalculations = "calculations-delete";
 
             public const string DeleteCalculationsPoisoned = "calculations-delete/$DeadLetterQueue";
 
-            public const string DeleteCalculationsPoisonedLocal = "calculations-delete-poisoned";
+            public const string DeleteCalculationsPoisonedLocal = "calculations-delete-poison";
 
             public const string DeleteCalculationResults = "calculation-results-delete";
 
             public const string DeleteCalculationResultsPoisoned = "calculation-results-delete/$DeadLetterQueue";
 
-            public const string DeleteCalculationResultsPoisonedLocal = "calculation-results-delete-poisoned";
+            public const string DeleteCalculationResultsPoisonedLocal = "calculation-results-delete-poison";
 
             public const string DeleteDatasets = "datasets-delete";
 
             public const string DeleteDatasetsPoisoned = "datasets-delete/$DeadLetterQueue";
 
-            public const string DeleteDatasetsPoisonedLocal = "datasets-delete-poisoned";
+            public const string DeleteDatasetsPoisonedLocal = "datasets-delete-poison";
 
             public const string DeleteTestResults = "test-results-delete";
 
             public const string DeleteTestResultsPoisoned = "test-results-delete/$DeadLetterQueue";
 
-            public const string DeleteTestResultsPoisonedLocal = "test-results-delete-poisoned";
+            public const string DeleteTestResultsPoisonedLocal = "test-results-delete-poison";
 
             public const string DeleteTests = "tests-delete";
 
             public const string DeleteTestsPoisoned = "tests-delete/$DeadLetterQueue";
 
-            public const string DeleteTestsPoisonedLocal = "tests-delete-poisoned";
+            public const string DeleteTestsPoisonedLocal = "tests-delete-poison";
 
             public const string DeleteSpecifications = "specifications-delete";
 
             public const string DeleteSpecificationsPoisoned = "specifications-delete/$DeadLetterQueue";
 
-            public const string DeleteSpecificationsPoisonedLocal = "specifications-delete-poisoned";
+            public const string DeleteSpecificationsPoisonedLocal = "specifications-delete-poison";
 
             public const string ReIndexSingleSpecification = "publishing-reindex-specification";
             
@@ -157,25 +157,25 @@
 
             public const string DeletePublishedProvidersPoisoned = "publishing-delete-published-providers/$DeadLetterQueue";
 
-            public const string DeletePublishedProvidersPoisonedLocal = "publishing-delete-published-providers-poisoned";
+            public const string DeletePublishedProvidersPoisonedLocal = "publishing-delete-published-providers-poison";
 
             public const string ReIndexSpecificationCalculationRelationships = "calculations-reindex-specification-calculation-rels";
 
             public const string ReIndexSpecificationCalculationRelationshipsPoisoned = "calculations-reindex-specification-calculation-rels/$DeadLetterQueue";
             
-            public const string ReIndexSpecificationCalculationRelationshipsPoisonedLocal = "calculations-reindex-specification-calculation-rels-poisoned";
+            public const string ReIndexSpecificationCalculationRelationshipsPoisonedLocal = "calculations-reindex-specification-calculation-rels-poison";
 
             public const string GeneratePublishedFundingCsv = "publishing-generate-published-funding-csv";
             
             public const string GeneratePublishedFundingCsvPoisoned = "publishing-generate-published-funding-csv/$DeadLetterQueue";
             
-            public const string GeneratePublishedFundingCsvPoisonedLocal = "publishing-generate-published-funding-csv-poisoned";
+            public const string GeneratePublishedFundingCsvPoisonedLocal = "publishing-generate-published-funding-csv-poison";
 
             public const string GeneratePublishedProviderEstateCsv = "publishing-generate-published-provider-estate-csv";
                                                  
             public const string GeneratePublishedProviderEstateCsvPoisoned = "publishing-generate-published-provider-estate-csv/$DeadLetterQueue";
                                                  
-            public const string GeneratePublishedProviderEstateCsvPoisonedLocal = "publishing-generate-published-provider-estate-csv-poisoned";
+            public const string GeneratePublishedProviderEstateCsvPoisonedLocal = "publishing-generate-published-provider-estate-csv-poison";
             
             public const string PublishedFundingUndo = "publishing-published-funding-undo";
 
@@ -183,31 +183,31 @@
 
             public const string PolicyReIndexTemplatesPoisoned = "policy-reindex-templates/$DeadLetterQueue";
 
-            public const string PolicyReIndexTemplatesPoisonedLocal = "policy-reindex-templates-poisoned";
+            public const string PolicyReIndexTemplatesPoisonedLocal = "policy-reindex-templates-poison";
 
             public const string MergeSpecificationInformationForProvider = "merge-specification-information-for-provider-with-results";
             
             public const string MergeSpecificationInformationForProviderPoisoned = "merge-specification-information-for-provider-with-results/$DeadLetterQueue";
             
-            public const string MergeSpecificationInformationForProviderPoisonedLocal = "merge-specification-information-poisoned";
+            public const string MergeSpecificationInformationForProviderPoisonedLocal = "merge-specification-information-poison";
 
             public const string ProviderSnapshotDataLoad = "provider-snapshot-data-load";
 
             public const string ProviderSnapshotDataLoadPoisoned = "provider-snapshot-data-load/$DeadLetterQueue";
 
-            public const string ProviderSnapshotDataLoadPoisonedLocal = "provider-snapshot-data-load-poisoned";
+            public const string ProviderSnapshotDataLoadPoisonedLocal = "provider-snapshot-data-load-poison";
 
             public const string UpdateCodeContextCache = "code-context-update";
             
             public const string UpdateCodeContextCachePoisoned = "code-context-update/$DeadLetterQueue";
             
-            public const string UpdateCodeContextCachePoisonedLocal = "code-context-update-poisoned";
+            public const string UpdateCodeContextCachePoisonedLocal = "code-context-update-poison";
 
             public const string SearchIndexWriter = "search-index-writer";
 
             public const string SearchIndexWriterPoisoned = "search-index-writer/$DeadLetterQueue";
             
-            public const string SearchIndexWriterPoisonedLocal = "search-index-writer-poisoned";
+            public const string SearchIndexWriterPoisonedLocal = "search-index-writer-poison";
         }
 
         public static class TopicNames
