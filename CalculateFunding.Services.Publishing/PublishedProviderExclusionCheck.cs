35e5f882dc785cee3ff645c2b4ec30fcc491b9a3
Dan Symonds-Lloyd <Daniel.SYMONDS-LLOYD@education.gov.uk>
2020-11-19 09:05:57 +0000

    Merged PR 9702: Changed exclusion check to require all cash calculations to be null, instead...
    Changed exclusion check to require all cash calculations to be null, instead of any
    
    Related work items: #55296

--- CalculateFunding.Services.Publishing/PublishedProviderExclusionCheck.cs
+++ CalculateFunding.Services.Publishing/PublishedProviderExclusionCheck.cs
@@ -2,15 +2,14 @@ using System.Collections.Generic;
 using System.Linq;
 using CalculateFunding.Common.ApiClient.Calcs.Models;
 using CalculateFunding.Models.Publishing;
-using TemplateFundingLine = CalculateFunding.Common.TemplateMetadata.Models.FundingLine;
 
 namespace CalculateFunding.Services.Publishing
 {
     public class PublishedProviderExclusionCheck : IPublishProviderExclusionCheck
     {
         public PublishedProviderExclusionCheckResult ShouldBeExcluded(
-            ProviderCalculationResult providerCalculationResult, 
-            TemplateMapping templateMapping, 
+            ProviderCalculationResult providerCalculationResult,
+            TemplateMapping templateMapping,
             Common.TemplateMetadata.Models.Calculation[] flattedCalculations)
         {
             IEnumerable<uint> cashCalculationTemplateIds = flattedCalculations
@@ -18,12 +17,12 @@ namespace CalculateFunding.Services.Publishing
                 .Select(f => f.TemplateCalculationId);
 
             IEnumerable<string> cashCalculationIds = templateMapping.TemplateMappingItems
-                .Where(c => c.EntityType == TemplateMappingEntityType.Calculation 
+                .Where(c => c.EntityType == TemplateMappingEntityType.Calculation
                             && cashCalculationTemplateIds.Contains(c.TemplateId))
                 .Select(f => f.CalculationId);
 
             bool shouldBeExcluded = providerCalculationResult.Results
-                .Any(c => cashCalculationIds.Contains(c.Id) && c.Value == null);
+                .All(c => cashCalculationIds.Contains(c.Id) && c.Value == null);
 
             return new PublishedProviderExclusionCheckResult(providerCalculationResult.ProviderId, shouldBeExcluded);
         }
