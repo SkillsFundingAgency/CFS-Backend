4c9e904efa44962f13a26b8c7fce19c6bec4adbc
Sinem OZTURK <Sinem.OZTURK@education.gov.uk>
2021-06-18 10:35:22 +0000

    Merged PR 12243: Add users function unit test project
    Add users function unit test project
    
    Related work items: #66064

---
+++ CalculateFunding.Functions.Users.UnitTests/IocConfigTests.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Reflection;
+using CalculateFunding.Functions.Users.ServiceBus;
+using CalculateFunding.Tests.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculateFunding.Functions.Users.UnitTests
+{
+    [TestClass]
+    public class IocConfigTests : FunctionIoCUnitTestBase
+    {
+        protected override Dictionary<string, string> AddToConfiguration()
+        {
+            Dictionary<string, string> configData = new Dictionary<string, string>
+            {
+                { "SearchServiceName", "ss-t1te-cfs"},
+                { "SearchServiceKey", "test" },
+                { "CosmosDbSettings:DatabaseName", "calculate-funding" },
+                { "CosmosDbSettings:ContainerName", "calcs" },
+                { "CosmosDbSettings:ConnectionString", "AccountEndpoint=https://test.documents.azure.com:443/;AccountKey=dGVzdA==;" },
+                { "specificationsClient:ApiEndpoint", "https://localhost:7001/api/" },
+                { "specificationsClient:ApiKey", "Local" },
+                { "resultsClient:ApiEndpoint", "https://localhost:7005/api/" },
+                { "resultsClient:ApiKey", "Local" },
+                { "calcsClient:ApiEndpoint", "https://localhost:7002/api" },
+                { "calcsClient:ApiKey", "Local" },
+                { "jobsClient:ApiEndpoint", "https://localhost:7010/api/"},
+                { "jobsClient:ApiKey", "Local"},
+                { "providersClient:ApiEndpoint", "https://localhost:7011/api/" },
+                { "providersClient:ApiKey", "Local" },
+                { "datasetsClient:ApiEndpoint", "https://localhost:7011/api/" },
+                { "datasetsClient:ApiKey", "Local" },
+                { "graphClient:ApiEndpoint", "https://localhost:7015/api/" },
+                { "graphClient:ApiKey", "Local" },
+            };
+
+            return configData;
+        }
+
+        protected override Assembly EntryAssembly => typeof(OnEditSpecificationEvent).Assembly;
+
+        protected override void RegisterDependencies()
+        {
+            Startup.RegisterComponents(ServiceCollection, CreateTestConfiguration());
+        }
+    }
+}
