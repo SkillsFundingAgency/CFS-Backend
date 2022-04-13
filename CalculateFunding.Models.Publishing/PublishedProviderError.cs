0df7cbd01ef7da3556ad325f94aaba83424af740
TATE, Matthew <Matthew.TATE@education.gov.uk>
2022-04-13 20:10:57 +0000

    Merged PR 16334: Make sure that when we detect whether the published provider version has chan...
    Make sure that when we detect whether the published provider version has changed we implement IComparable.
    
    Related work items: #93458

--- CalculateFunding.Models.Publishing/PublishedProviderError.cs
+++ CalculateFunding.Models.Publishing/PublishedProviderError.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace CalculateFunding.Models.Publishing
 {
-    public class PublishedProviderError
+    public class PublishedProviderError : IComparable
     {
         public string Identifier { get; set; }
         
@@ -13,5 +15,28 @@ namespace CalculateFunding.Models.Publishing
         public string FundingLineCode { get; set; }
 
         public string FundingStreamId { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (GetHashCode() < obj?.GetHashCode()) return -1;
+            if (GetHashCode() == obj?.GetHashCode()) return 0;
+            return 1;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return GetHashCode().Equals(obj?.GetHashCode());
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(
+                Identifier,
+                Type,
+                SummaryErrorMessage,
+                DetailedErrorMessage,
+                FundingLineCode,
+                FundingStreamId );
+        }
     }
 }
