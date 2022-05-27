4abe430013ad7057402e4945f7a0492d20825d37
TATE, Matthew <Matthew.TATE@education.gov.uk>
2022-05-27 14:42:47 +0000

    Merged PR 16995: Create a new api which queues relevant publishing report jobs based on the re...
    Create a new api which queues relevant publishing report jobs based on the requested action.
    
    Related work items: #96846

---
+++ CalculateFunding.Api.Publishing/Controllers/PublishingReportController.cs
@@ -0,0 +1,44 @@
+﻿using CalculateFunding.Common.ApiClient.Jobs.Models;
+using CalculateFunding.Common.Models;
+using CalculateFunding.Services.Core.Extensions;
+using CalculateFunding.Services.Publishing.Interfaces;
+using CalculateFunding.Services.Publishing.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CalculateFunding.Api.Publishing.Controllers
+{
+    [ApiController]
+    public class PublishingReportController : ControllerBase
+    {
+        private readonly IPublishedFundingCsvJobsService _publishFundingCsvJobsService;
+
+        public PublishingReportController(
+                    IPublishedFundingCsvJobsService publishFundingCsvJobsService)
+        {
+            _publishFundingCsvJobsService = publishFundingCsvJobsService;
+        }
+
+        /// <summary>
+        /// Queue report jobs for target specification for selected action
+        /// </summary>
+        /// <param name="createAction">The type of report jobs to queue Refresh, Approve or Release</param>
+        /// <param name="specificationId">Target specification to queue reporting jobs from</param>
+        /// <returns></returns>
+        [HttpGet("api/specifications/{createAction}/{specificationId}/queue-report-jobs")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Job>))]
+        public async Task<IActionResult> QueueReportJobs(
+            [FromRoute] GeneratePublishingCsvJobsCreationAction createAction,
+            [FromRoute] string specificationId)
+        {
+            Reference user = ControllerContext.HttpContext.Request.GetUserOrDefault();
+            string correlationId = ControllerContext.HttpContext.Request.GetCorrelationId();
+            
+            return new OkObjectResult(await _publishFundingCsvJobsService.QueueCsvJobs(createAction,
+                specificationId,
+                correlationId,
+                user));
+        }
+    }
+}
