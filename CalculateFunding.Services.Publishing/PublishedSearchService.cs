06d408ce71a3524399b669d454b1b3f2e38b3cb1
Suresh RAMASWAMY <Suresh.RAMASWAMY@EDUCATION.GOV.UK>
2022-08-26 13:42:35 +0000

    Merged PR 18334: BE code ayesha changes for view provider results
    BE code ayesha changes for view provider results
    
    Related work items: #92493

--- CalculateFunding.Services.Publishing/PublishedSearchService.cs
+++ CalculateFunding.Services.Publishing/PublishedSearchService.cs
@@ -356,7 +356,7 @@ namespace CalculateFunding.Services.Publishing
                             IsIndicative = m.Result.IsIndicative,
                             HasErrors = m.Result.HasErrors,
                             Errors = m.Result.Errors,
-                            OpenedDate = m.Result.DateOpened,
+                            DateOpened = m.Result.DateOpened,
                             MajorVersion = m.Result.MajorVersion,
                             MinorVersion = m.Result.MinorVersion,
                             ReleaseChannels = releaseChannelLookupByProviderId != null ? 
